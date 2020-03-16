3d6fd23bfb1507d8949767c06eb2d0010bdd7306
docmarcelo <dsrslps@gmail.com>
2020-03-16 03:16:35 -0300

    Tdos os arquivos desenvolvidos até a data do commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="" id="app">
+
+    <div class="container grid-xs py-2">
+    <img class="img-responsive img-logo" src="@/assets/logo.png" alt="logo marca da suite-case">
+    <form @submit.prevent="addTdo(todo)">
+      <div class="input-group">
+        <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo" name="" id="">
+        <button class="btn btn-primary input-group-btn">Adicionar</button>
+      </div>
+    </form>
+        <div class="todo-list">
+        <todo v-for="t in todos" :key="t.id" :todo="t" />
+
+        </div>
+      </div>
+    </div>
+  
+</template>
+
+<script>
+import Todo from './components/Todo';
+
+export default{
+  name: "app",
+  components: { Todo },
+  data() {
+    return { todos: [], todo:{cheked: false} };
+    },
+  methods:{
+      addTdo(todo){
+        todo.id = Date.now();
+        this.todos.push(todo);
+        this.todo = {cheked:false};
+      }
+  }
+};
+</script>
+
+<style scoped>
+.img-logo{
+  max-width: 200px;
+  margin: 0 auto;
+  padding-bottom: 20px;
+
+}
+  
+</style>
+  
---
+++ src/components/Todo.vue
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+<template>
+     <div class="tile flex-centered">
+        <div class="tile-icon">
+            <i class="icon icon-time"></i>
+        </div>
+          <div class="tile-content">
+            <div class="tile-subtitle">{{ todo.description }}</div>
+          </div>
+          <div class="tile-action">
+            <button class="btn btn-link">Concluído</button>
+            <button class="btn btn-link">
+            <span class="text-error">Remover</span>
+            </button>
+          </div>
+     </div>  
+       
+</template>
+
+
+<script>
+export default {
+    props: {
+        todo: { type: Object, required: true }
+        }
+};
+</script>
